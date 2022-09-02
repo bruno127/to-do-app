f8b864c0634e0533402fafe28d92266ef3df75ad
bruno127 <bvasilj@gmail.com>
2022-09-02 17:43:53 +0200

    added text-field

--- src/App.vue
+++ src/App.vue
@@ -50,21 +50,9 @@
       @click="drawer = !drawer"
       ></v-app-bar-nav-icon>
 
-      <v-app-bar-title>Title</v-app-bar-title>
+      <v-app-bar-title>Todo app</v-app-bar-title>
 
       <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
