541a61991abac3ea3574e7ff611e3b4d09357388
bruno127 <bvasilj@gmail.com>
2022-09-02 16:16:45 +0200

    added routes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="pa-6">
+    <h1>About Todo app</h1>
+    <p>App made with Vue.js using Vuetify</p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="home pa-6">
+  <h1>Todo app</h1>
+</div>
+</template>
+
+<script>
+
+
+  export default {
+
+  }
+</script>
