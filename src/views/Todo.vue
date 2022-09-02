844bd92faac700bb8d42f0823d0ba733a6302fbe
bruno127 <bvasilj@gmail.com>
2022-09-02 16:57:05 +0200

    added line trough decoration

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -19,7 +19,10 @@
           ></v-checkbox>
         </v-list-item-action>
         <v-list-item-content>
-          <v-list-item-title> {{ task.title }}</v-list-item-title>
+          <v-list-item-title
+          :class="{ 'text-decoration-line-through' : task.done }"
+          > 
+          {{ task.title }}</v-list-item-title>
         </v-list-item-content>
       </template>
     </v-list-item>
